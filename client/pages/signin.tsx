478cf980dfa7d49c8bfaec89af7e90c5a5a4f1a9
nnha19 <nyinyihmueaung19@gmail.com>
2021-12-07 20:30:55 +0630

    module not found error on vercel

--- client/pages/signin.tsx
+++ client/pages/signin.tsx
@@ -1,10 +1,10 @@
-// import Auth from "../Components/Auth/Auth";
+import Auth from "../Components/Auth/Auth";
 
 const Signin = () => {
   return (
     <>
       <h1>Hello World</h1>
-      {/* <Auth authMode="signin" /> */}
+      <Auth authMode="signin" />
     </>
   );
 };
--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,10 +1,10 @@
-// import Auth from "../Components/Auth/Auth";
+import Auth from "../Components/Auth/Auth";
 
 const Signup = () => {
   return (
     <>
       <h2>Sign UP.</h2>
-      {/* <Auth authMode="signup" /> */}
+      <Auth authMode="signup" />
     </>
   );
 };
