29c42ebee5c50c41993c2abc1ac7b5ca6a14de36
nnha19 <nyinyihmueaung19@gmail.com>
2021-12-08 12:27:37 +0630

    deploy again

--- client/pages/users.tsx
+++ client/pages/users.tsx
@@ -1,31 +1,12 @@
 import axios from "axios";
 import React from "react";
 
-const Users = ({ users }) => {
-  const usersList = users.map((u) => (
-    <div key={u._id}>
-      <p>{u.username}</p>
-    </div>
-  ));
-
+const Users = () => {
   return (
     <div>
       <h1>List of users</h1>
-      {usersList}
     </div>
   );
-};
-
-export const getStaticProps = async () => {
-  const resp = await axios({
-    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
-    method: "GET",
-  });
-  console.log(resp);
-
-  return {
-    props: { users: resp.data },
-  };
 };
 
 export default Users;
