70a1f3041f02b3ec22ee37ac0ff28456b9cf0242
nnha19 <nyinyihmueaung19@gmail.com>
2021-12-08 21:00:57 +0630

    ui button changes if fri request already sent

--- client/components/Users/Users.tsx
+++ client/components/Users/Users.tsx
@@ -5,11 +5,32 @@ import { IUsers } from "../../types/types";
 import PrimaryBtn from "../Share/PrimaryBtn/PrimaryBtn";
 import styles from "./Users.module.scss";
 
+interface IUserFriReqs {
+  userId: string;
+  friendRequests: string[];
+  sentRequests: string[];
+}
+
 const Users = () => {
   const { user } = useAppSelector((state) => state.user);
   const [allUsers, setAllUsers] = useState<IUsers["users"]>();
-  const [curUserFriReqs, setCurUserFriReqs] = useState();
+  const [curUserFriReqs, setCurUserFriReqs] = useState<IUserFriReqs>();
+  const [loading, setLoading] = useState(false);
+  const [secLoading, setSecLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
+    (async () => {
+      const resp = await axios({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${user._id}/request`,
+      });
+      setCurUserFriReqs(resp.data);
+      setLoading(false);
+    })();
+  }, [user]);
+
+  useEffect(() => {
+    setSecLoading(true);
     (async () => {
       const resp = await axios({
         url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
@@ -17,11 +38,25 @@ const Users = () => {
       });
       setAllUsers(resp.data);
     })();
+    setSecLoading(false);
   }, [user]);
+
+  const handleAddFriend = async (uid: string, rid: string) => {
+    const resp = await axios({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${uid}/request/${rid}`,
+      method: "POST",
+    });
+    console.log(resp.data);
+  };
 
   const usersList =
     allUsers?.length > 0 &&
+    !loading &&
+    !secLoading &&
     allUsers.map((u) => {
+      const reqAlreadySent = curUserFriReqs.sentRequests.some(
+        (uid) => uid === u._id
+      );
       return (
         <div className={styles.user}>
           <img
@@ -31,7 +66,9 @@ const Users = () => {
           <div className={styles.userInfo}>
             <h2 className={styles.userName}>{u.username}</h2>
             <div className={styles.userBtns}>
-              <PrimaryBtn>Add Friend</PrimaryBtn>
+              <PrimaryBtn onClick={() => handleAddFriend(user._id, u._id)}>
+                {!reqAlreadySent ? "Add Friend" : "Pending..."}
+              </PrimaryBtn>
               <PrimaryBtn>Remove</PrimaryBtn>
             </div>
           </div>
@@ -39,7 +76,11 @@ const Users = () => {
       );
     });
 
-  return <div className={styles.users}>{usersList}</div>;
+  return (
+    <div className={styles.users}>
+      {loading || secLoading ? "Loading..." : usersList}
+    </div>
+  );
 };
 
 export default Users;
